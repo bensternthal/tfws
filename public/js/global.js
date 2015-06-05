a872fdb1f1d4b0e000e7a3a42d3a9c51a5fa0c1a
Benjamin Sternthal <ben@devpatch.com>
2015-06-04 20:28:41 -0700

    Changed to gauges.

--- public/js/global.js
+++ public/js/global.js
@@ -2,51 +2,41 @@
 
 (function () {
   var socket = io();
-  var timestamp =  ((new Date()).getTime() / 1000)|0;
-
-  var lineChartData = [
-    {
-      label: "Series 1",
-      values: [ {time: timestamp, y: 0} ]
-    }
-  ];
 
   var humidity = $('#humidity').epoch({
-    type: 'time.line',
-    historySize: 50,
-    data: lineChartData,
-    axes: ['left', 'bottom', 'right']
+    type: 'time.gauge',
+    domain: [0, 100],
+    ticks: 50,
+    format: function(v) { return (v).toFixed(1) + ' %'; }
   });
 
   var airpress = $('#airpress').epoch({
-    type: 'time.line',
-    historySize: 50,
-    data: lineChartData,
-    axes: ['left', 'bottom', 'right']
+    type: 'time.gauge',
+    domain: [0, 1500],
+    ticks: 50,
+    format: function(v) { return (v).toFixed(3) + ' hPa'; }
   });
 
   var temp = $('#temp').epoch({
-    type: 'time.line',
-    historySize: 50,
-    data: lineChartData,
-    axes: ['left', 'bottom', 'right']
+    type: 'time.gauge',
+    domain: [0, 50],
+    ticks: 50,
+    format: function(v) { return (v).toFixed(2) + ' °C'; }
   });
 
   var illuminance = $('#illuminance').epoch({
-    type: 'time.line',
-    historySize: 50,
-    data: lineChartData,
-    axes: ['left', 'bottom', 'right']
+    type: 'time.gauge',
+    domain: [0, 500],
+    ticks: 50,
+    format: function(v) { return (v).toFixed(1) + ' Lux';}
   });
 
+
   socket.on('tfdata', function (data) {
-    var mytime =  parseInt(((new Date()).getTime() / 1000)|0);
-
-    humidity.push([{time: mytime, y: data.humidity}]);
-    airpress.push([{time: mytime, y: data.air_pressure}]);
-    temp.push([{time: mytime, y: data.temperature}]);
-    illuminance.push([{time: mytime, y: data.illuminance}]);
-
+   humidity.push(data.humidity);
+   airpress.push(data.air_pressure);
+   temp.push(data.temperature);
+   illuminance.push(data.illuminance);
   });
 
 })();
